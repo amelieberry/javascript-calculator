c33a28dba5bd9644dd2d816273e447b3797493ae
Amelie <aac.berry@gmail.com>
2023-09-29 14:26:56 +0200

    moved all data to a single array

--- src/data/data.js
+++ src/data/data.js
@@ -1,76 +1,70 @@
-export const numPad = [
-    {
-      text: '0',
-      id: 'zero'
-    },
-    {
-      text: '.',
-      id: 'decimal'
-    },
-    {
-      text: '1',
-      id: 'one'
-    },
-    {
-      text: '2',
-      id: 'two'
-    },
-    {
-      text: '3',
-      id: 'three'
-    },
-    {
-      text: '4',
-      id: 'four'
-    },
-    {
-      text: '5',
-      id: 'five'
-    },
-    {
-      text: '6',
-      id: 'six'
-    },
-    {
-      text: '7',
-      id: 'seven'
-    },
-    {
-      text: '8',
-      id: 'eight'
-    },
-    {
-      text: '9',
-      id: 'nine'
-    }
-  ]
-  
-  export const operators = [
-    {
-      text: '+',
-      id: 'add'
-    },
-    {
-      text: '-',
-      id: 'subtract'
-    },
-    {
-      text: '*',
-      id: 'multiply'
-    },
-    {
-      text: '/',
-      id: 'divide'
-    },
-  ]
-
-  export const actionsPad = [
-    {
-        text: 'AC',
-        id: 'clear'
-    },
-    {
-        text: '=',
-        id: 'equals'
-    }
+export const keyPad = [
+  {
+    text: '7',
+    id: 'seven'
+  },
+  {
+    text: '8',
+    id: 'eight'
+  },
+  {
+    text: '9',
+    id: 'nine'
+  },
+  {
+    text: '/',
+    id: 'divide'
+  },
+  {
+    text: '4',
+    id: 'four'
+  },
+  {
+    text: '5',
+    id: 'five'
+  },
+  {
+    text: '6',
+    id: 'six'
+  },
+  {
+    text: '*',
+    id: 'multiply'
+  },
+  {
+    text: '1',
+    id: 'one'
+  },
+  {
+    text: '2',
+    id: 'two'
+  },
+  {
+    text: '3',
+    id: 'three'
+  },
+  {
+    text: '-',
+    id: 'subtract'
+  },
+  {
+    text: '0',
+    id: 'zero'
+  },
+  {
+    text: '.',
+    id: 'decimal'
+  },
+  {
+    text: '+',
+    id: 'add'
+  },
+  {
+    text: 'AC',
+    id: 'clear'
+},
+{
+    text: '=',
+    id: 'equals'
+} 
   ]
